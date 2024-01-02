e39d2eab590104ad9725e5f44aae2627285f4291
KenStarry <starrycodes@gmail.com>
2024-01-02 14:17:42 +0300

    Added Dark Theme Data

--- lib/theme/my_theme.dart
+++ lib/theme/my_theme.dart
@@ -23,4 +23,25 @@ class MyTheme {
         titleLarge: TextStyle(
             fontSize: 26, fontWeight: FontWeight.bold, color: textBlack900),
       ));
+
+  /// Dark Theme
+  static ThemeData get darkTheme => ThemeData(
+      primaryColor: accent,
+      scaffoldBackgroundColor: bgColorDarkMode,
+      iconTheme: const IconThemeData(color: textWhite300),
+      useMaterial3: true,
+      textTheme: const TextTheme(
+        bodySmall: TextStyle(
+            fontSize: 11, fontWeight: FontWeight.w500, color: textWhite500),
+        bodyMedium: TextStyle(
+            fontSize: 14, fontWeight: FontWeight.w500, color: textWhite500),
+        bodyLarge: TextStyle(
+            fontSize: 16, fontWeight: FontWeight.w500, color: textWhite700),
+        titleSmall: TextStyle(
+            fontSize: 18, fontWeight: FontWeight.w700, color: textWhite700),
+        titleMedium: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: textWhite900),
+        titleLarge: TextStyle(
+            fontSize: 26, fontWeight: FontWeight.bold, color: textWhite900),
+      ));
 }
