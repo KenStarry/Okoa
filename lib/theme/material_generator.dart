9f70e03c3f01d1d602869100bb48ac780d43188c
KenStarry <starrycodes@gmail.com>
2024-01-02 14:25:05 +0300

    Get tint value method implementation

--- lib/theme/material_generator.dart
+++ lib/theme/material_generator.dart
@@ -2,3 +2,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 /// Generate Material Colors
+MaterialColor generateMaterialColor(Color color) =>
+    MaterialColor(color.value, {});
+
+/// Tint Generator
+int tintValue(int value, double factor) =>
+    max(0, min((value + ((255 - value) * factor)).round(), 255));
