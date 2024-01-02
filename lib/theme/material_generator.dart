902c8f1b5108b96d91ea8fbcc3f9ad57d992b2af
KenStarry <starrycodes@gmail.com>
2024-01-02 14:28:12 +0300

    Shade generator implementation

--- lib/theme/material_generator.dart
+++ lib/theme/material_generator.dart
@@ -16,3 +16,11 @@ Color tintColor(Color color, double factor) => Color.fromRGBO(
     1);
 
 /// Shade Generator
+int shadeValue(int value, double factor) =>
+    max(0, min(value - (value * factor).round(), 255));
+
+Color shadeColor(Color color, double factor) => Color.fromRGBO(
+    shadeValue(color.red, factor),
+    shadeValue(color.green, factor),
+    shadeValue(color.blue, factor),
+    1);
