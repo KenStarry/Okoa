59ecd633bb1f2e1a4c1f8d826acad5052cbb268c
KenStarry <starrycodes@gmail.com>
2024-01-02 14:41:12 +0300

    Overwrite Logo images for both pngs and svgs

--- lib/features/feature_auth/presentation/login/login_page.dart
+++ lib/features/feature_auth/presentation/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:okoa/theme/colors.dart';
+import 'package:okoa/theme/material_generator.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,10 +13,11 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: false,
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        color: Theme.of(context).primaryColor,
+        color: generateMaterialColor(accent).shade50,
       ),
     );
   }
