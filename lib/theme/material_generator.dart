2dfae991dd9142a9dba7b4d3e68ef773c2545e28
KenStarry <starrycodes@gmail.com>
2024-01-02 14:26:56 +0300

    Generate Tint

--- lib/theme/material_generator.dart
+++ lib/theme/material_generator.dart
@@ -8,3 +8,11 @@ MaterialColor generateMaterialColor(Color color) =>
 /// Tint Generator
 int tintValue(int value, double factor) =>
     max(0, min((value + ((255 - value) * factor)).round(), 255));
+
+Color tintColor(Color color, double factor) => Color.fromRGBO(
+    tintValue(color.red, factor),
+    tintValue(color.green, factor),
+    tintValue(color.blue, factor),
+    1);
+
+/// Shade Generator
