3c0af30000d0be15d64e24622a24964192b44872
KenStarry <starrycodes@gmail.com>
2024-01-04 00:21:06 +0300

    Create account button toggles the various screens

--- lib/features/feature_auth/presentation/login/login_page.dart
+++ lib/features/feature_auth/presentation/login/login_page.dart
@@ -2,8 +2,11 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:okoa/core/presentation/extensions/build_context_extensions.dart';
 import 'package:okoa/features/feature_auth/presentation/components/auth_card.dart';
+import 'package:okoa/features/feature_auth/presentation/controller/auth_controller.dart';
 import 'package:okoa/theme/colors.dart';
 import 'package:okoa/theme/material_generator.dart';
 
@@ -15,13 +18,24 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late final AuthController _authController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _authController = Get.find<AuthController>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
       value: SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
           statusBarIconBrightness:
-              context.isDarkMode ? Brightness.light : Brightness.dark),
+              BuildContextExtensions(Get.context!).isDarkMode
+                  ? Brightness.light
+                  : Brightness.dark),
       child: Scaffold(
         resizeToAvoidBottomInset: true,
         extendBodyBehindAppBar: false,
@@ -39,7 +53,7 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 24),
                 //  logo
                 SvgPicture.asset(
-                    context.isDarkMode
+                    BuildContextExtensions(Get.context!).isDarkMode
                         ? "assets/images/logo_alt_dark_mode.svg"
                         : "assets/images/logo_alt_light_mode.svg",
                     width: 150),
@@ -73,7 +87,10 @@ class _LoginPageState extends State<LoginPage> {
                                     .bodyMedium!
                                     .fontWeight,
                                 color: Theme.of(context).primaryColor),
-                            recognizer: TapGestureRecognizer()..onTap = () {}),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                _authController.setIsLogin(isLogin: false);
+                              }),
                       ])),
                     ],
                   ),
