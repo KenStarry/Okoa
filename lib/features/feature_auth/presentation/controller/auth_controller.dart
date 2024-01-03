5af7cd7252da75975cc8b46ed1c85ddb6f7359f4
KenStarry <starrycodes@gmail.com>
2024-01-04 01:43:53 +0300

    Sign up method implemented in auth controller

--- lib/features/feature_auth/presentation/controller/auth_controller.dart
+++ lib/features/feature_auth/presentation/controller/auth_controller.dart
@@ -1,8 +1,17 @@
 import 'package:get/get.dart';
+import 'package:okoa/di/di.dart';
+import 'package:okoa/features/feature_auth/domain/use_case/auth_use_cases.dart';
 
 class AuthController extends GetxController {
+  final authUseCase = locator.get<AuthUseCases>();
+
   //  current screen
   final isLogin = false.obs;
 
   void setIsLogin({required bool isLogin}) => this.isLogin.value = isLogin;
+
+  /// Sign Up
+  Future<void> signUp(
+          {required String email, required String password}) async =>
+      await authUseCase.signUp.call(email: email, password: password);
 }
