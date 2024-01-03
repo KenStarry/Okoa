3691fa15c15cfdf1d4444e4cf6f9c85c5d2548da
KenStarry <starrycodes@gmail.com>
2024-01-03 15:10:18 +0300

    Modified status bar color

--- lib/features/feature_auth/presentation/login/components/login_card.dart
+++ lib/features/feature_auth/presentation/login/components/login_card.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:okoa/features/feature_auth/presentation/login/components/login_card_clipper.dart';
-import 'package:okoa/theme/colors.dart';
-import 'package:okoa/theme/material_generator.dart';
 
 class LoginCard extends StatelessWidget {
   const LoginCard({super.key});
--- lib/features/feature_auth/presentation/login/login_page.dart
+++ lib/features/feature_auth/presentation/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:okoa/core/presentation/extensions/build_context_extensions.dart';
 import 'package:okoa/features/feature_auth/presentation/login/components/login_card.dart';
@@ -15,28 +16,33 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: false,
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        padding: const EdgeInsets.all(16),
-        color: Theme.of(context).scaffoldBackgroundColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            //  logo
-            SvgPicture.asset(
-              context.isDarkMode
-                  ? "assets/images/logo_alt_dark_mode.svg"
-                  : "assets/images/logo_alt_light_mode.svg",
-              width: 150
-            ),
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness:
+              context.isDarkMode ? Brightness.light : Brightness.dark),
+      child: Scaffold(
+        extendBodyBehindAppBar: false,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: const EdgeInsets.all(16),
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //  logo
+              SvgPicture.asset(
+                  context.isDarkMode
+                      ? "assets/images/logo_alt_dark_mode.svg"
+                      : "assets/images/logo_alt_light_mode.svg",
+                  width: 150),
 
-            //  login card
-            LoginCard()
-          ],
+              //  login card
+              LoginCard()
+            ],
+          ),
         ),
       ),
     );
