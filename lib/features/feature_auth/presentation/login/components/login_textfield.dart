3b4a27afd0883f417230dfc9a2ca0b26526c851f
KenStarry <starrycodes@gmail.com>
2024-01-03 16:01:39 +0300

    Modified login form text field style

--- lib/features/feature_auth/presentation/login/components/login_textfield.dart
+++ lib/features/feature_auth/presentation/login/components/login_textfield.dart
@@ -8,10 +8,27 @@ class LoginTextField extends StatefulWidget {
 }
 
 class _LoginTextFieldState extends State<LoginTextField> {
+  late final TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-
+      controller: _textEditingController,
+      obscureText: false,
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyMedium,
+      decoration: InputDecoration(
+        hintText: "Enter your email",
+        hintStyle: Theme.of(context).textTheme.bodyMedium,
+      ),
+      onChanged: (value){},
     );
   }
 }
