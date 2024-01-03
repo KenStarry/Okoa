a00e26f87825e941e682697560eb4576e7c31c05
KenStarry <starrycodes@gmail.com>
2024-01-03 15:05:33 +0300

    Modified Login Card

--- lib/features/feature_auth/presentation/login/components/login_card.dart
+++ lib/features/feature_auth/presentation/login/components/login_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:okoa/features/feature_auth/presentation/login/components/login_card_clipper.dart';
+import 'package:okoa/theme/colors.dart';
+import 'package:okoa/theme/material_generator.dart';
 
 class LoginCard extends StatelessWidget {
   const LoginCard({super.key});
@@ -10,9 +12,9 @@ class LoginCard extends StatelessWidget {
     return ClipPath(
       clipper: LoginCardClipper(),
       child: Container(
-        height: 500,
-        width: 300,
-        color: Colors.blue,
+        height: 400,
+        width: double.infinity,
+        color: generateMaterialColor(accent).shade100,
       ),
     );
   }
