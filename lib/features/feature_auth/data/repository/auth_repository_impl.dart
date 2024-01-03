5bf17208c518d2f194d59dd848ab0ead9dfaa802
KenStarry <starrycodes@gmail.com>
2024-01-04 01:36:51 +0300

    Sign up implementation

--- lib/features/feature_auth/data/repository/auth_repository_impl.dart
+++ lib/features/feature_auth/data/repository/auth_repository_impl.dart
@@ -3,13 +3,19 @@ import 'package:okoa/features/feature_auth/domain/repository/auth_repository.dar
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
-
   final supabase = locator.get<SupabaseClient>();
 
   @override
-  Future<void> signUp({required String email, required String password}) {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      final response =
+          await supabase.auth.signUp(email: email, password: password);
 
+      if (response.session != null) {
+        print("SUCCESSFUL SIGN UP!!");
+      }
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
 }
