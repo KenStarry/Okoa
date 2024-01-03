9f6bd151e9c5206a7c2e59aec8f7cc0f409341f4
KenStarry <starrycodes@gmail.com>
2024-01-04 01:41:56 +0300

    Added Auth use cases to dependency injection

--- lib/di/auth_di.dart
+++ lib/di/auth_di.dart
@@ -1,11 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:okoa/features/feature_auth/data/repository/auth_repository_impl.dart';
 import 'package:okoa/features/feature_auth/domain/repository/auth_repository.dart';
+import 'package:okoa/features/feature_auth/domain/use_case/auth_use_cases.dart';
+import 'package:okoa/features/feature_auth/domain/use_case/sign_up.dart';
 
 void authDI({required GetIt locator}) {
-
   /// Repository
   locator.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
 
   /// Use Cases
+  locator.registerLazySingleton<AuthUseCases>(
+      () => AuthUseCases(signUp: SignUp()));
 }
