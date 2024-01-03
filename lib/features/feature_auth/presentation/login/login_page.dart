09ebe852e7fcfed206b8717c16f897f02bce54e9
KenStarry <starrycodes@gmail.com>
2024-01-03 20:23:07 +0300

    Implemented back button

--- lib/features/feature_auth/presentation/login/login_page.dart
+++ lib/features/feature_auth/presentation/login/login_page.dart
@@ -50,29 +50,44 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 24),
 
                 //  Create account text
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RichText(
-                        text: TextSpan(children: [
-                      TextSpan(
-                          text: "No account? ",
-                          style: Theme.of(context).textTheme.bodyMedium),
-                      TextSpan(
-                          text: "Create one.",
-                          style: TextStyle(
-                              fontSize: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .fontSize,
-                              fontWeight: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .fontWeight,
-                              color: Theme.of(context).primaryColor),
-                          recognizer: TapGestureRecognizer()..onTap = () {}),
-                    ])),
-                  ],
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        child: const Center(
+                          child: Icon(Icons.arrow_back_rounded),
+                        ),
+                      ),
+                      RichText(
+                          text: TextSpan(children: [
+                        TextSpan(
+                            text: "No account? ",
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        TextSpan(
+                            text: "Create one.",
+                            style: TextStyle(
+                                fontSize: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .fontSize,
+                                fontWeight: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .fontWeight,
+                                color: Theme.of(context).primaryColor),
+                            recognizer: TapGestureRecognizer()..onTap = () {}),
+                      ])),
+                    ],
+                  ),
                 )
               ],
             ),
