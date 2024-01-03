da716af6836b5feebf866214d78dc8399a6f438a
KenStarry <starrycodes@gmail.com>
2024-01-03 15:25:40 +0300

    Added Profile pic icon to login card

--- lib/features/feature_auth/presentation/login/components/login_card.dart
+++ lib/features/feature_auth/presentation/login/components/login_card.dart
@@ -6,12 +6,38 @@ class LoginCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: LoginCardClipper(),
-      child: Container(
-        height: 400,
-        width: double.infinity,
-        color: Theme.of(context).primaryColorLight,
+    return Container(
+      color: Colors.transparent,
+      child: Stack(
+        children: [
+          ClipPath(
+            clipper: LoginCardClipper(),
+            child: Container(
+              height: 400,
+              width: double.infinity,
+              decoration:
+                  BoxDecoration(color: Theme.of(context).primaryColorLight),
+              child: Container(width: double.infinity, height: double.infinity),
+            ),
+          ),
+
+          //  profile pic icon
+          Positioned(
+            left: (MediaQuery.of(context).size.width * 0.5) - 50,
+            top: 15,
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColorLight,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      width: 3,
+                      strokeAlign: BorderSide.strokeAlignOutside)),
+            ),
+          )
+        ],
       ),
     );
   }
