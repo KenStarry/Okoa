ca76f953ba63d22ab027f45e01282f46bd2d7c45
KenStarry <starrycodes@gmail.com>
2024-01-04 16:14:40 +0300

    Auth Use cases implementation

--- lib/di/auth_di.dart
+++ lib/di/auth_di.dart
@@ -3,7 +3,9 @@ import 'package:okoa/features/feature_auth/data/repository/auth_repository_impl.
 import 'package:okoa/features/feature_auth/domain/repository/auth_repository.dart';
 import 'package:okoa/features/feature_auth/domain/use_case/auth_subscription.dart';
 import 'package:okoa/features/feature_auth/domain/use_case/auth_use_cases.dart';
+import 'package:okoa/features/feature_auth/domain/use_case/get_auth_user.dart';
 import 'package:okoa/features/feature_auth/domain/use_case/sign_in.dart';
+import 'package:okoa/features/feature_auth/domain/use_case/sign_out.dart';
 import 'package:okoa/features/feature_auth/domain/use_case/sign_up.dart';
 
 void authDI({required GetIt locator}) {
@@ -14,5 +16,7 @@ void authDI({required GetIt locator}) {
   locator.registerLazySingleton<AuthUseCases>(() => AuthUseCases(
       signUp: SignUp(),
       signIn: SignIn(),
+      signOut: SignOut(),
+      getAuthUser: GetAuthUser(),
       authSubscription: AuthSubscription()));
 }
