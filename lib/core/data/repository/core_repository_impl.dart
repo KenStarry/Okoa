a83c12c1abc35024ff5eb097742edd4c34bfcac1
KenStarry <starrycodes@gmail.com>
2024-01-07 15:04:28 +0300

    Core repository implementation

---
+++ lib/core/data/repository/core_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:okoa/core/domain/repository/core_repository.dart';
+import 'package:okoa/features/feature_auth/domain/model/okoa_user.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../../di/di.dart';
+
+class CoreRepositoryimpl extends CoreRepository {
+  final supabase = locator.get<SupabaseClient>();
+
+  @override
+  Future<void> getUserDataFromDatabase(
+      {required String uid,
+      required Function(OkoaUser p1) onGetUserData}) async {
+    try {
+      final userData =
+          await supabase.from('users').select().eq('id', uid).single();
+
+      print("----------USER DATA : $userData");
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+}
