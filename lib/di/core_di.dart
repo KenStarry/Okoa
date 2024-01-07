e87db3f3b3aaae57b4f1eda5baa247b89c25ba54
KenStarry <starrycodes@gmail.com>
2024-01-07 15:05:39 +0300

    Provided core repository in DI

--- lib/di/core_di.dart
+++ lib/di/core_di.dart
@@ -1,8 +1,13 @@
 import 'package:get_it/get_it.dart';
+import 'package:okoa/core/data/repository/core_repository_impl.dart';
+import 'package:okoa/core/domain/repository/core_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void coreDI({required GetIt locator}) {
   
   /// Supabase Client
   locator.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
+
+  /// Core Repository
+  locator.registerLazySingleton<CoreRepository>(() => CoreRepositoryimpl());
 }
