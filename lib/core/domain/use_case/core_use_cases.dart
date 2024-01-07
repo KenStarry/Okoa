9e543e8c684195ff1b53db72ded7983afd392428
KenStarry <starrycodes@gmail.com>
2024-01-07 15:08:20 +0300

    Added Core Use cases

---
+++ lib/core/domain/use_case/core_use_cases.dart
@@ -0,0 +1,7 @@
+import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
+
+class CoreUseCases {
+  final GetUserDataFromDB getUserDataFromDB;
+
+  CoreUseCases({required this.getUserDataFromDB});
+}
