214b4a70400cb8b1d733b69cde7be419341ea5e8
KenStarry <starrycodes@gmail.com>
2024-01-07 23:25:44 +0300

    Added SOS buttonon SOS section

--- lib/features/feature_home/presentation/components/sos_status_section.dart
+++ lib/features/feature_home/presentation/components/sos_status_section.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 class SOSStatusSection extends StatefulWidget {
@@ -18,7 +19,21 @@ class _SOSStatusSectionState extends State<SOSStatusSection> {
           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(100)),
           color: Theme.of(context).primaryColor,
         ),
-
+        child: Center(
+          child: AvatarGlow(
+            glowShape: BoxShape.circle,
+            glowCount: 3,
+            glowRadiusFactor: 0.3,
+            glowColor: Colors.greenAccent,
+            child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.greenAccent),
+            ),
+          ),
+        ),
       ),
     );
   }
