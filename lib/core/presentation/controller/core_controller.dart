399825d4ce93289868281a4fb0a2b428b802e57f
KenStarry <starrycodes@gmail.com>
2024-01-07 15:13:34 +0300

    Implemented Core controller

---
+++ lib/core/presentation/controller/core_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:okoa/core/domain/use_case/core_use_cases.dart';
+import 'package:okoa/di/di.dart';
+import 'package:okoa/features/feature_auth/domain/model/okoa_user.dart';
+
+class CoreController extends GetxController {
+  final useCase = locator.get<CoreUseCases>();
+
+  /// User Data
+  final okoaUser = Rxn<OkoaUser>();
+
+  //  get user data from DB
+  Future<void> getUserDataFromDatabase(
+          {required String uid,
+          required Function(OkoaUser okoaUser) onGetUserData}) async =>
+      useCase.getUserDataFromDB.call(uid: uid, onGetUserData: onGetUserData);
+}
