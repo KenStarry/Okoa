c40aa8b17fed2f862536c1f7f65e22783ff1cb8d
KenStarry <starrycodes@gmail.com>
2024-01-07 14:53:14 +0300

    Added User Id as parameter in core repository

--- lib/core/domain/repository/core_repository.dart
+++ lib/core/domain/repository/core_repository.dart
@@ -3,5 +3,5 @@ import 'package:okoa/features/feature_auth/domain/model/okoa_user.dart';
 abstract class CoreRepository {
   /// Get User Data
   Future<void> getUserDataFromDatabase(
-      {required Function(OkoaUser) onGetUserData});
+      {required String uid, required Function(OkoaUser) onGetUserData});
 }
