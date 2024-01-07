27e665445e1766a1cfb5d76775592fe9a602640d
KenStarry <starrycodes@gmail.com>
2024-01-07 16:11:31 +0300

    Listen to user data use case implementation

--- lib/core/domain/use_case/core_use_cases.dart
+++ lib/core/domain/use_case/core_use_cases.dart
@@ -1,7 +1,10 @@
 import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
+import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
 
 class CoreUseCases {
   final GetUserDataFromDB getUserDataFromDB;
+  final ListenToUserDataOnDB listenToUserDataOnDB;
 
-  CoreUseCases({required this.getUserDataFromDB});
+  CoreUseCases(
+      {required this.getUserDataFromDB, required this.listenToUserDataOnDB});
 }
--- lib/di/core_di.dart
+++ lib/di/core_di.dart
@@ -3,6 +3,7 @@ import 'package:okoa/core/data/repository/core_repository_impl.dart';
 import 'package:okoa/core/domain/repository/core_repository.dart';
 import 'package:okoa/core/domain/use_case/core_use_cases.dart';
 import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
+import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void coreDI({required GetIt locator}) {
@@ -13,6 +14,7 @@ void coreDI({required GetIt locator}) {
   locator.registerLazySingleton<CoreRepository>(() => CoreRepositoryimpl());
 
   /// Core Use Cases
-  locator.registerLazySingleton<CoreUseCases>(
-      () => CoreUseCases(getUserDataFromDB: GetUserDataFromDB()));
+  locator.registerLazySingleton<CoreUseCases>(() => CoreUseCases(
+      getUserDataFromDB: GetUserDataFromDB(),
+      listenToUserDataOnDB: ListenToUserDataOnDB()));
 }
