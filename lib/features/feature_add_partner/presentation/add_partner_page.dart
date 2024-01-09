560d69eb06e20ee99e656c7837448d21a39e6646
KenStarry <starrycodes@gmail.com>
2024-01-10 01:52:39 +0300

    Add partner page

---
+++ lib/features/feature_add_partner/presentation/add_partner_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPartnerPage extends StatefulWidget {
+  const AddPartnerPage({super.key});
+
+  @override
+  State<AddPartnerPage> createState() => _AddPartnerPageState();
+}
+
+class _AddPartnerPageState extends State<AddPartnerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
