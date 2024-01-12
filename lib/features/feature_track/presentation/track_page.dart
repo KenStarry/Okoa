bba2d6cf460ff47535845a690233d3f90291b468
KenStarry <starrycodes@gmail.com>
2024-01-12 15:43:02 +0300

    Added Map image

--- lib/features/feature_track/presentation/track_page.dart
+++ lib/features/feature_track/presentation/track_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:okoa/features/feature_track/presentation/components/track_location_page.dart';
 import 'package:okoa/features/feature_track/presentation/components/track_map.dart';
 
 class TrackPage extends StatefulWidget {
@@ -18,7 +19,12 @@ class _TrackPageState extends State<TrackPage> {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        child: TrackMap(),
+        child: const Stack(
+          children: [
+            TrackMap(),
+            TrackLocationPage()
+          ],
+        ),
       ),
     );
   }
