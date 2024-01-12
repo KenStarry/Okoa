04ef5593c2dd080aaf24ee6a6570cc89177b51b0
KenStarry <starrycodes@gmail.com>
2024-01-12 15:52:53 +0300

    Location page design implementation

--- lib/features/feature_track/presentation/components/track_location_page.dart
+++ lib/features/feature_track/presentation/components/track_location_page.dart
@@ -14,13 +14,24 @@ class _TrackLocationPageState extends State<TrackLocationPage> {
     return Container(
       width: double.infinity,
       height: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
       color: Theme.of(context).scaffoldBackgroundColor,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Text("Location", style: Theme.of(context).textTheme.titleMedium),
+          const SizedBox(height: 24),
           //  map image
-          SvgPicture.asset("assets/images/map.svg", width: 200, height: 200)
+          SvgPicture.asset("assets/images/map.svg", width: 200, height: 200),
+
+          const SizedBox(height: 24),
+
+          Text(
+            "Grant location access to enable Okoa keep you safe.",
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          )
         ],
       ),
     );
