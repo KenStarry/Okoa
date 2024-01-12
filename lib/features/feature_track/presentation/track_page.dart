c86b7eabb8bd244017050b829bb07d41e7c5a578
KenStarry <starrycodes@gmail.com>
2024-01-12 14:51:30 +0300

    Updated Android API Key

--- lib/features/feature_track/presentation/track_page.dart
+++ lib/features/feature_track/presentation/track_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrackPage extends StatefulWidget {
   const TrackPage({super.key});
@@ -8,8 +9,19 @@ class TrackPage extends StatefulWidget {
 }
 
 class _TrackPageState extends State<TrackPage> {
+  static const LatLng sourceLocation =
+      LatLng(-1.2663447549789835, 36.837615802117085);
+  static const LatLng targetLocation =
+      LatLng(37.42796133580664, -122.085749655962);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition:
+              CameraPosition(target: sourceLocation, zoom: 14.5)),
+    );
   }
 }
