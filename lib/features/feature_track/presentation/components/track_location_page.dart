a8f2d8e7be20e7450d3ca477399b28b1d513ed91
KenStarry <starrycodes@gmail.com>
2024-01-12 15:33:09 +0300

    Added Track location page

---
+++ lib/features/feature_track/presentation/components/track_location_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TrackLocationPage extends StatefulWidget {
+  const TrackLocationPage({super.key});
+
+  @override
+  State<TrackLocationPage> createState() => _TrackLocationPageState();
+}
+
+class _TrackLocationPageState extends State<TrackLocationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.redAccent,
+    );
+  }
+}
