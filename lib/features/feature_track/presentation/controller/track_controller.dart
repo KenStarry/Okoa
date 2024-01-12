7dab49f43c2ed57305d626d9b8d3a4e25fb80f2d
KenStarry <starrycodes@gmail.com>
2024-01-12 16:13:13 +0300

    Check Location permission status and request permission implementation

--- lib/features/feature_track/presentation/controller/track_controller.dart
+++ lib/features/feature_track/presentation/controller/track_controller.dart
@@ -4,6 +4,23 @@ import 'package:permission_handler/permission_handler.dart';
 class TrackController extends GetxController {
   final locationPermissionStatus = PermissionStatus.denied.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+
+    checkLocationPermissionStatus();
+  }
+
   void checkLocationPermissionStatus() async =>
       locationPermissionStatus.value = await Permission.location.status;
+
+  void requestLocationPermission() {
+    Permission.location
+        .onGrantedCallback(
+            () => locationPermissionStatus.value = PermissionStatus.granted)
+        .onDeniedCallback(
+            () => locationPermissionStatus.value = PermissionStatus.denied)
+        .onPermanentlyDeniedCallback(() => locationPermissionStatus.value =
+            PermissionStatus.permanentlyDenied);
+  }
 }
