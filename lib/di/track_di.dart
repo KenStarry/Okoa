5e63d4f7cd7d540f00afc7037a69e05eb7037d1f
KenStarry <starrycodes@gmail.com>
2024-01-13 14:18:34 +0300

    Added Track use Case implementation

--- lib/di/track_di.dart
+++ lib/di/track_di.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:location/location.dart';
 import 'package:okoa/features/feature_track/data/repository/track_repository_impl.dart';
 import 'package:okoa/features/feature_track/domain/repository/track_repository.dart';
+import 'package:okoa/features/feature_track/domain/use_case/request_location_service.dart';
+import 'package:okoa/features/feature_track/domain/use_case/track_use_case.dart';
 
 void trackDI({required GetIt locator}) {
   /// Location
@@ -9,4 +11,8 @@ void trackDI({required GetIt locator}) {
 
   /// Track Repo
   locator.registerLazySingleton<TrackRepository>(() => TrackRepositoryImpl());
+
+  /// Track Use Cases
+  locator.registerLazySingleton<TrackUseCase>(
+      () => TrackUseCase(requestLocationService: RequestLocationService()));
 }
---
+++ lib/features/feature_track/domain/use_case/track_use_case.dart
@@ -0,0 +1,7 @@
+import 'package:okoa/features/feature_track/domain/use_case/request_location_service.dart';
+
+class TrackUseCase {
+  final RequestLocationService requestLocationService;
+
+  TrackUseCase({required this.requestLocationService});
+}
