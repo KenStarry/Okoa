085a9f8864e779d76fc06a9cfb2a462ad4fb4eea
KenStarry <starrycodes@gmail.com>
2024-01-13 14:33:45 +0300

    Import tweaks in track controller

--- lib/features/feature_track/presentation/controller/track_controller.dart
+++ lib/features/feature_track/presentation/controller/track_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:location/location.dart';
 import 'package:okoa/di/di.dart';
 import 'package:okoa/features/feature_track/domain/use_case/track_use_case.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart'
+    as permission_handler;
 
 class TrackController extends GetxController {
   final useCase = locator.get<TrackUseCase>();
-  final locationPermissionStatus = PermissionStatus.denied.obs;
+
+  final locationPermissionStatus =
+      permission_handler.PermissionStatus.denied.obs;
+
+  final currentLocation = Rxn<LocationData>();
 
   @override
   void onInit() {
@@ -15,16 +21,21 @@ class TrackController extends GetxController {
     requestLocationService();
   }
 
-  void checkLocationPermissionStatus() async =>
-      locationPermissionStatus.value = await Permission.location.status;
+  void checkLocationPermissionStatus() async => locationPermissionStatus.value =
+      await permission_handler.Permission.location.status;
 
   void requestLocationPermission() async {
-    final status = await Permission.location.request();
+    final status = await permission_handler.Permission.location.request();
 
     locationPermissionStatus.value = status;
   }
 
   /// Location Service
-  void requestLocationService() async =>
-      await useCase.requestLocationService();
+  void requestLocationService() async => await useCase.requestLocationService();
+
+  /// Listen to current location
+  void listenToCurrentLocation(
+          {required Function(LocationData locationData) onLocationChanged}) =>
+      useCase.listenToCurrentLocation
+          .call(onLocationChanged: onLocationChanged);
 }
