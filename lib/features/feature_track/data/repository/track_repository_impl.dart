4aae4aab967e5fc73aa730136f195663c4591007
KenStarry <starrycodes@gmail.com>
2024-01-13 14:11:30 +0300

    Check if location is enabled

---
+++ lib/features/feature_track/data/repository/track_repository_impl.dart
@@ -0,0 +1,10 @@
+import 'package:location/location.dart';
+import 'package:okoa/di/di.dart';
+import 'package:okoa/features/feature_track/domain/repository/track_repository.dart';
+
+class TrackRepositoryImpl extends TrackRepository {
+  final location = locator.get<Location>();
+
+  Future<bool> _checkLocationServiceEnabled() async =>
+      await location.serviceEnabled();
+}
