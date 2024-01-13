3ca21ea2c8bac3b1e50a3c8375b5c6743dd22058
KenStarry <starrycodes@gmail.com>
2024-01-13 14:30:34 +0300

    Listen to current location use case implementation

--- lib/di/track_di.dart
+++ lib/di/track_di.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:location/location.dart';
 import 'package:okoa/features/feature_track/data/repository/track_repository_impl.dart';
 import 'package:okoa/features/feature_track/domain/repository/track_repository.dart';
+import 'package:okoa/features/feature_track/domain/use_case/listen_to_current_location.dart';
 import 'package:okoa/features/feature_track/domain/use_case/request_location_service.dart';
 import 'package:okoa/features/feature_track/domain/use_case/track_use_case.dart';
 
@@ -13,6 +14,7 @@ void trackDI({required GetIt locator}) {
   locator.registerLazySingleton<TrackRepository>(() => TrackRepositoryImpl());
 
   /// Track Use Cases
-  locator.registerLazySingleton<TrackUseCase>(
-      () => TrackUseCase(requestLocationService: RequestLocationService()));
+  locator.registerLazySingleton<TrackUseCase>(() => TrackUseCase(
+      requestLocationService: RequestLocationService(),
+      listenToCurrentLocation: ListenToCurrentLocation()));
 }
--- lib/features/feature_track/domain/use_case/track_use_case.dart
+++ lib/features/feature_track/domain/use_case/track_use_case.dart
@@ -1,7 +1,11 @@
+import 'package:okoa/features/feature_track/domain/use_case/listen_to_current_location.dart';
 import 'package:okoa/features/feature_track/domain/use_case/request_location_service.dart';
 
 class TrackUseCase {
   final RequestLocationService requestLocationService;
+  final ListenToCurrentLocation listenToCurrentLocation;
 
-  TrackUseCase({required this.requestLocationService});
+  TrackUseCase(
+      {required this.requestLocationService,
+      required this.listenToCurrentLocation});
 }
