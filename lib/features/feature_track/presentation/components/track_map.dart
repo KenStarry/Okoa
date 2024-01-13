d2ad044e21100bf5189d44e2c326a0738a3fa001
KenStarry <starrycodes@gmail.com>
2024-01-13 17:20:03 +0300

    Build Markers method implementation

--- lib/features/feature_track/presentation/components/track_map.dart
+++ lib/features/feature_track/presentation/components/track_map.dart
@@ -15,6 +15,10 @@ class TrackMap extends StatefulWidget {
 class _TrackMapState extends State<TrackMap> {
   late final TrackController _trackController;
   late final Completer<GoogleMapController> _googleMapController;
+  late final List<Map<String, dynamic>> markersData;
+  final Map<String, Marker> _markers = {};
+
+  bool isLoaded = false;
 
   @override
   void initState() {
@@ -22,6 +26,20 @@ class _TrackMapState extends State<TrackMap> {
 
     _trackController = Get.find<TrackController>();
     _googleMapController = Completer<GoogleMapController>();
+
+    markersData = [
+      {
+        'id': '1',
+        'globalKey': GlobalKey(),
+        'widget': Container(
+          width: 50,
+          height: 50,
+          color: Colors.redAccent,
+        )
+      }
+    ];
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => _buildMarkers());
 
     ever(_trackController.currentLocation, (currentLocation) async {
       if (currentLocation != null) {
@@ -42,24 +60,40 @@ class _TrackMapState extends State<TrackMap> {
     return Obx(
       () {
         final currentUserLocation = _trackController.currentLocation.value!;
-        return GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: CameraPosition(
-                target: LatLng(currentUserLocation.latitude!,
-                    currentUserLocation.longitude!),
-                zoom: 14.5),
-            myLocationButtonEnabled: true,
-            myLocationEnabled: true,
-            onMapCreated: (GoogleMapController controller) {
-              _googleMapController.complete(controller);
-            },
-            markers: {
-              Marker(
-                  markerId: MarkerId("currentLocation"),
-                  position: LatLng(currentUserLocation.latitude!,
-                      currentUserLocation.longitude!))
-            });
+        return isLoaded
+            ? GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(currentUserLocation.latitude!,
+                        currentUserLocation.longitude!),
+                    zoom: 14.5),
+                myLocationButtonEnabled: true,
+                myLocationEnabled: true,
+                onMapCreated: (GoogleMapController controller) {
+                  _googleMapController.complete(controller);
+                },
+                markers: {
+                    Marker(
+                        markerId: MarkerId("currentLocation"),
+                        position: LatLng(currentUserLocation.latitude!,
+                            currentUserLocation.longitude!))
+                  })
+            : RepaintBoundary(
+                key: markersData[0]['globalKey'],
+                child: markersData[0]['widget'],
+              );
       },
     );
+  }
+
+  Future<void> _buildMarkers() async {
+    await Future.wait(markersData.map((data) async {
+      Marker marker = await _generateMarkerFromWidget(data: data);
+      _markers[marker.markerId.value] = marker;
+    }));
+
+    setState(() {
+      isLoaded = true;
+    });
   }
 }
