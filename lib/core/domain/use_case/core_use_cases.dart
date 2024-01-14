e3b0967a41b8ac4d6c50422c2c3db3b54966b59f
KenStarry <starrycodes@gmail.com>
2024-01-14 16:26:22 +0300

    Uppdate user data on db use case

--- lib/core/domain/use_case/core_use_cases.dart
+++ lib/core/domain/use_case/core_use_cases.dart
@@ -1,10 +1,14 @@
 import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
 import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
+import 'package:okoa/core/domain/use_case/update_user_data_on_db.dart';
 
 class CoreUseCases {
   final GetUserDataFromDB getUserDataFromDB;
+  final UpdateUserDataOnDB updateUserDataOnDB;
   final ListenToUserDataOnDB listenToUserDataOnDB;
 
   CoreUseCases(
-      {required this.getUserDataFromDB, required this.listenToUserDataOnDB});
+      {required this.getUserDataFromDB,
+      required this.updateUserDataOnDB,
+      required this.listenToUserDataOnDB});
 }
--- lib/di/core_di.dart
+++ lib/di/core_di.dart
@@ -4,6 +4,7 @@ import 'package:okoa/core/domain/repository/core_repository.dart';
 import 'package:okoa/core/domain/use_case/core_use_cases.dart';
 import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
 import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
+import 'package:okoa/core/domain/use_case/update_user_data_on_db.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void coreDI({required GetIt locator}) {
@@ -16,5 +17,6 @@ void coreDI({required GetIt locator}) {
   /// Core Use Cases
   locator.registerLazySingleton<CoreUseCases>(() => CoreUseCases(
       getUserDataFromDB: GetUserDataFromDB(),
+      updateUserDataOnDB: UpdateUserDataOnDB(),
       listenToUserDataOnDB: ListenToUserDataOnDB()));
 }
