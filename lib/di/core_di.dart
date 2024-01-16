29aabb9aaa0d39a2c9fd2dd051104250fc17733b
KenStarry <starrycodes@gmail.com>
2024-01-16 22:57:06 +0300

    Listen to internet status implementation

--- lib/di/core_di.dart
+++ lib/di/core_di.dart
@@ -3,6 +3,7 @@ import 'package:okoa/core/data/repository/core_repository_impl.dart';
 import 'package:okoa/core/domain/repository/core_repository.dart';
 import 'package:okoa/core/domain/use_case/core_use_cases.dart';
 import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
+import 'package:okoa/core/domain/use_case/listen_to_internet_status.dart';
 import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
 import 'package:okoa/core/domain/use_case/update_user_data_on_db.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -18,5 +19,6 @@ void coreDI({required GetIt locator}) {
   locator.registerLazySingleton<CoreUseCases>(() => CoreUseCases(
       getUserDataFromDB: GetUserDataFromDB(),
       updateUserDataOnDB: UpdateUserDataOnDB(),
-      listenToUserDataOnDB: ListenToUserDataOnDB()));
+      listenToUserDataOnDB: ListenToUserDataOnDB(),
+      listenToInternetStatus: ListenToInternetStatus()));
 }
