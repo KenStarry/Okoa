7322c1d6b9a12b817a868b89046c358569c4b219
KenStarry <starrycodes@gmail.com>
2024-01-16 22:56:22 +0300

    Listen to internet status implementation

--- lib/core/domain/use_case/core_use_cases.dart
+++ lib/core/domain/use_case/core_use_cases.dart
@@ -2,13 +2,17 @@ import 'package:okoa/core/domain/use_case/get_user_data_from_db.dart';
 import 'package:okoa/core/domain/use_case/listen_to_user_data_on_db.dart';
 import 'package:okoa/core/domain/use_case/update_user_data_on_db.dart';
 
+import 'listen_to_internet_status.dart';
+
 class CoreUseCases {
   final GetUserDataFromDB getUserDataFromDB;
   final UpdateUserDataOnDB updateUserDataOnDB;
   final ListenToUserDataOnDB listenToUserDataOnDB;
+  final ListenToInternetStatus listenToInternetStatus;
 
   CoreUseCases(
       {required this.getUserDataFromDB,
       required this.updateUserDataOnDB,
-      required this.listenToUserDataOnDB});
+      required this.listenToUserDataOnDB,
+      required this.listenToInternetStatus});
 }
