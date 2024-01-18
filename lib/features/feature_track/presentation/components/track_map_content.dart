96554d9dd07477f7c0f9caf6227b2d8e84c0b7de
KenStarry <starrycodes@gmail.com>
2024-01-18 22:28:22 +0300

    Partner card implementatation

--- lib/features/feature_track/presentation/components/track_map_content.dart
+++ lib/features/feature_track/presentation/components/track_map_content.dart
@@ -55,7 +55,7 @@ class _TrackMapContentState extends State<TrackMapContent> {
               color: Theme.of(context).scaffoldBackgroundColor,
               child: ListView.separated(
                 itemBuilder: (context, index) => const TrackPartnerCard(),
-                separatorBuilder: (context, index) => const SizedBox(width: 20),
+                separatorBuilder: (context, index) => const SizedBox(width: 24),
                 itemCount: 10,
                 scrollDirection: Axis.horizontal,
                 physics: const BouncingScrollPhysics(),
