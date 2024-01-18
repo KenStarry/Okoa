6c20d8448d25d3192dc25bf86e0f2988b7285c70
KenStarry <starrycodes@gmail.com>
2024-01-19 01:11:30 +0300

    Partner controller implementation

--- lib/di/controllers_di.dart
+++ lib/di/controllers_di.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:okoa/core/presentation/controller/core_controller.dart';
+import 'package:okoa/features/feature_add_partner/presentation/controller/partner_controller.dart';
 import 'package:okoa/features/feature_auth/presentation/controller/auth_controller.dart';
 import 'package:okoa/features/feature_main/presentation/controller/main_controller.dart';
 import 'package:okoa/features/feature_track/presentation/controller/track_controller.dart';
@@ -10,4 +11,5 @@ void initializeControllers() {
   Get.lazyPut(() => CoreController(), fenix: true);
   Get.lazyPut(() => MainController(), fenix: true);
   Get.lazyPut(() => TrackController(), fenix: true);
+  Get.lazyPut(() => PartnerController(), fenix: true);
 }
