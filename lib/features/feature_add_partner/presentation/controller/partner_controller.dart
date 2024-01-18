fc7951cbe24f3096bc0a7631cab9536a65987060
KenStarry <starrycodes@gmail.com>
2024-01-19 01:20:09 +0300

    Check contacts permission if granted

--- lib/features/feature_add_partner/presentation/controller/partner_controller.dart
+++ lib/features/feature_add_partner/presentation/controller/partner_controller.dart
@@ -6,7 +6,13 @@ import 'package:permission_handler/permission_handler.dart'
 class PartnerController extends GetxController {
   final contactsPermissionGranted = false.obs;
 
-  Future<void> requestContactPermission() async {
+  void checkContactPermission() async {
+    final status = await permission_handler.Permission.contacts.status;
+
+    contactsPermissionGranted.value = status.isGranted;
+  }
+
+  void requestContactPermission() async {
     final status = await permission_handler.Permission.contacts.request();
 
     contactsPermissionGranted.value = status.isGranted;
