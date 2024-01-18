9242bfe21bde98d691e4ca95325c5ddaf852ffe8
KenStarry <starrycodes@gmail.com>
2024-01-19 01:14:47 +0300

    Request contact permission function

--- lib/features/feature_add_partner/presentation/controller/partner_controller.dart
+++ lib/features/feature_add_partner/presentation/controller/partner_controller.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:get/get.dart';
 
 class PartnerController extends GetxController {
+  final contactsPermissionGranted = false.obs;
 
-
-
+  Future<void> requestContactPermission() async =>
+      contactsPermissionGranted.value =
+          await FlutterContacts.requestPermission();
 }
