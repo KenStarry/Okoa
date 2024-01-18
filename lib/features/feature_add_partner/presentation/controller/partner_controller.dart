4889b65e68b561f2163c2ed6c781adf36ed72a13
KenStarry <starrycodes@gmail.com>
2024-01-19 01:17:27 +0300

    Request permission implementation using permission handler

--- lib/features/feature_add_partner/presentation/controller/partner_controller.dart
+++ lib/features/feature_add_partner/presentation/controller/partner_controller.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart'
+    as permission_handler;
 
 class PartnerController extends GetxController {
   final contactsPermissionGranted = false.obs;
 
-  Future<void> requestContactPermission() async =>
-      contactsPermissionGranted.value =
-          await FlutterContacts.requestPermission();
+  Future<void> requestContactPermission() async {
+    final status = await permission_handler.Permission.contacts.request();
+
+    contactsPermissionGranted.value = status.isGranted;
+  }
 }
