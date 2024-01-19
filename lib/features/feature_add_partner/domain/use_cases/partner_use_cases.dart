99491eacb8005381a2828ad45afa49ae28e30788
KenStarry <starrycodes@gmail.com>
2024-01-19 10:58:44 +0300

    Partner use cases

--- lib/features/feature_add_partner/domain/use_cases/partner_use_cases.dart
+++ lib/features/feature_add_partner/domain/use_cases/partner_use_cases.dart
@@ -1,3 +1,14 @@
-class PartnerUseCases {
+import 'package:okoa/features/feature_add_partner/domain/use_cases/check_contact_permission.dart';
+import 'package:okoa/features/feature_add_partner/domain/use_cases/get_contacts.dart';
+import 'package:okoa/features/feature_add_partner/domain/use_cases/request_contact_permission.dart';
 
+class PartnerUseCases {
+  final CheckContactPermission checkContactPermission;
+  final RequestContactPermission requestContactPermission;
+  final GetContactsUseCase getContactsUseCase;
+
+  PartnerUseCases(
+      {required this.checkContactPermission,
+      required this.requestContactPermission,
+      required this.getContactsUseCase});
 }
