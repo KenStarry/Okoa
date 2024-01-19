7394552240ffd9e47bd435c657db7d9789ae2f8f
KenStarry <starrycodes@gmail.com>
2024-01-19 10:50:28 +0300

    Get Contacts use case

--- lib/features/feature_add_partner/domain/use_cases/get_contacts.dart
+++ lib/features/feature_add_partner/domain/use_cases/get_contacts.dart
@@ -1,3 +1,10 @@
-class GetContactsUseCase {
+import 'package:flutter_contacts/contact.dart';
+import 'package:okoa/di/di.dart';
+import 'package:okoa/features/feature_add_partner/domain/repository/partner_repository.dart';
 
+class GetContactsUseCase {
+  final repo = locator.get<PartnerRepository>();
+
+  void call({required Function(List<Contact> contacts) onContactsFetched}) =>
+      repo.getContacts(onContactsFetched: onContactsFetched);
 }
