008500a63afd9c12393c4c3d729991c3ff02d2b0
KenStarry <starrycodes@gmail.com>
2024-01-19 10:43:21 +0300

    Partner repository implementation

--- lib/features/feature_add_partner/data/repository/partner_repository_impl.dart
+++ lib/features/feature_add_partner/data/repository/partner_repository_impl.dart
@@ -1,24 +1,39 @@
 import 'package:flutter_contacts/contact.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:okoa/features/feature_add_partner/domain/repository/partner_repository.dart';
+import 'package:permission_handler/permission_handler.dart'
+    as permission_handler;
 
 class PartnerRepositoryImpl extends PartnerRepository {
-
   ///  Check contacts permission
   @override
-  void checkContactPermission({required bool isGranted}) {
-    // TODO: implement checkContactPermission
+  void checkContactPermission(
+      {required Function(bool isGranted) onCheckPermission}) async {
+    final status = await permission_handler.Permission.contacts.status;
+
+    onCheckPermission(status.isGranted);
   }
 
   ///  Get Contacts
   @override
-  void getContacts({required Function(List<Contact> contacts) onContactsFetched}) {
-    // TODO: implement getContacts
+  void getContacts(
+      {required Function(List<Contact> contacts) onContactsFetched}) async {
+    checkContactPermission(onCheckPermission: (isGranted) async {
+      if (isGranted) {
+        final contacts =
+            await FlutterContacts.getContacts(withProperties: true);
+
+        onContactsFetched(contacts);
+      }
+    });
   }
 
   ///  Request contacts permission
   @override
-  void requestContactPermission({required bool isGranted}) {
-    // TODO: implement requestContactPermission
-  }
+  void requestContactPermission(
+      {required Function(bool isGranted) onRequestPermission}) async {
+    final status = await permission_handler.Permission.contacts.request();
 
+    onRequestPermission(status.isGranted);
+  }
 }
--- lib/features/feature_add_partner/domain/repository/partner_repository.dart
+++ lib/features/feature_add_partner/domain/repository/partner_repository.dart
@@ -2,10 +2,12 @@ import 'package:flutter_contacts/contact.dart';
 
 abstract class PartnerRepository {
   //  Check permission
-  void checkContactPermission({required bool isGranted});
+  void checkContactPermission(
+      {required Function(bool isGranted) onCheckPermission});
 
   //  Request contact permission
-  void requestContactPermission({required bool isGranted});
+  void requestContactPermission(
+      {required Function(bool isGranted) onRequestPermission});
 
   //  Get contacts
   void getContacts(
