4bfd418a6ba82397224d97e03c67c6d1005ed752
KenStarry <starrycodes@gmail.com>
2024-01-19 18:13:10 +0300

    Toggle Partner method implementation for contacts screen

--- lib/features/feature_add_partner/presentation/controller/partner_controller.dart
+++ lib/features/feature_add_partner/presentation/controller/partner_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:get/get.dart';
 import 'package:okoa/di/di.dart';
 import 'package:okoa/features/feature_add_partner/domain/use_cases/partner_use_cases.dart';
+import 'package:okoa/features/feature_auth/domain/model/okoa_user.dart';
 import 'package:permission_handler/permission_handler.dart'
     as permission_handler;
 
@@ -11,6 +12,7 @@ class PartnerController extends GetxController {
   final contactsPermissionGranted = false.obs;
 
   final contacts = Rxn<List<Contact>>();
+  final selectedPartners = <OkoaUser>[].obs;
 
   @override
   void onInit() {
@@ -33,4 +35,13 @@ class PartnerController extends GetxController {
       useCase.getContactsUseCase.call(onContactsFetched: (contacts) {
         this.contacts.value = contacts;
       });
+
+  //  select a partner
+  void togglePartner({required OkoaUser user}) {
+    if (selectedPartners.contains(user)) {
+      selectedPartners.removeWhere((myUser) => myUser.userId == user.userId);
+    } else {
+      selectedPartners.add(user);
+    }
+  }
 }
