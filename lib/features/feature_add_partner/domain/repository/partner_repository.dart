d21ca09b3c5ddb7b9d33de9f2a67694b0d1496fd
KenStarry <starrycodes@gmail.com>
2024-01-19 10:25:19 +0300

    Get contacts to partner repository implementation

--- lib/features/feature_add_partner/domain/repository/partner_repository.dart
+++ lib/features/feature_add_partner/domain/repository/partner_repository.dart
@@ -1,7 +1,13 @@
+import 'package:flutter_contacts/contact.dart';
+
 abstract class PartnerRepository {
   //  Check permission
   void checkContactPermission({required bool isGranted});
 
   //  Request contact permission
   void requestContactPermission({required bool isGranted});
+
+  //  Get contacts
+  void getContacts(
+      {required Function(List<Contact> contacts) onContactsFetched});
 }
