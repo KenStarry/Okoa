3c893ebdc939640ba150ac1507bf5461e09c16f4
KenStarry <starrycodes@gmail.com>
2024-01-21 01:01:47 +0300

    Alerts Content

--- lib/features/feature_alerts/presentation/alerts_page.dart
+++ lib/features/feature_alerts/presentation/alerts_page.dart
@@ -12,27 +12,40 @@ class _AlertsPageState extends State<AlertsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: false,
-      backgroundColor: Theme
-          .of(context)
-          .scaffoldBackgroundColor,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: SafeArea(
           child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            padding: const EdgeInsets.all(16),
-            color: Theme
-                .of(context)
-                .scaffoldBackgroundColor,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.all(16),
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //  alerts app bar
+            Row(
               children: [
-                //  alerts app bar
-                Text("Alerts", style: Theme.of(context).textTheme.titleLarge)
-                //  alerts content
-                //  navigation toolbar
+                Icon(Icons.add_alert_rounded,
+                    size: 32, color: Theme.of(context).primaryColor),
+                const SizedBox(width: 8),
+                Text("Alerts", style: Theme.of(context).textTheme.titleLarge),
               ],
             ),
-          )),
+
+            const SizedBox(height: 24),
+            //  alerts content
+            Expanded(child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.red
+              ),
+            ))
+            //  navigation toolbar
+          ],
+        ),
+      )),
     );
   }
 }
