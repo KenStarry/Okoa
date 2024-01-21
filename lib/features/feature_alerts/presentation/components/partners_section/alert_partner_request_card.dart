b885cc1d9227f8b392e436728330c933c7cafaea
KenStarry <starrycodes@gmail.com>
2024-01-21 15:29:42 +0300

    Alert card code adjustments

--- lib/features/feature_alerts/presentation/components/partners_section/alert_partner_request_card.dart
+++ lib/features/feature_alerts/presentation/components/partners_section/alert_partner_request_card.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:okoa/core/presentation/components/avatar.dart';
+import 'package:okoa/core/presentation/controller/core_controller.dart';
 import 'package:okoa/features/feature_add_partner/domain/model/okoa_partner.dart';
 
-class AlertPartnerRequestCard extends StatelessWidget {
+class AlertPartnerRequestCard extends StatefulWidget {
   final OkoaPartner partner;
 
   const AlertPartnerRequestCard({super.key, required this.partner});
+
+  @override
+  State<AlertPartnerRequestCard> createState() =>
+      _AlertPartnerRequestCardState();
+}
+
+class _AlertPartnerRequestCardState extends State<AlertPartnerRequestCard> {
+  late final CoreController _coreController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _coreController = Get.find<CoreController>();
+  }
 
   @override
   Widget build(BuildContext context) {
