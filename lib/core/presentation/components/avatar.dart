db16596d96b6d6c8fb8fca4fdc107bf8c829733d
KenStarry <starrycodes@gmail.com>
2024-01-21 15:15:51 +0300

    Added Avatar Url implementation

--- lib/core/presentation/components/avatar.dart
+++ lib/core/presentation/components/avatar.dart
@@ -1,10 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Avatar extends StatelessWidget {
-  const Avatar({super.key});
+  final String? avatarUrl;
+  final Size size;
+
+  const Avatar({super.key, required this.avatarUrl, required this.size});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: size.width,
+      height: size.height,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      child: avatarUrl == null || avatarUrl!.isEmpty
+          ? const Icon(Icons.person_rounded)
+          : ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: CachedNetworkImage(
+                imageUrl: avatarUrl!,
+                placeholder: (context, url) => CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+    );
   }
 }
