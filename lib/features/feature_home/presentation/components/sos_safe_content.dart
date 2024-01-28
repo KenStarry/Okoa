0d85fe372bd3a5455a86441dd200821d8546bcd4
KenStarry <starrycodes@gmail.com>
2024-01-28 20:42:35 +0300

    SOS Warning implementation

---
+++ lib/features/feature_home/presentation/components/sos_safe_content.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SosSafeContent extends StatefulWidget {
+  const SosSafeContent({super.key});
+
+  @override
+  State<SosSafeContent> createState() => _SosSafeContentState();
+}
+
+class _SosSafeContentState extends State<SosSafeContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("SOS Safe implementation");
+  }
+}
