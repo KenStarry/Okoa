01297a5df249d48dd45a225be5e6f0d4494aaa72
KenStarry <starrycodes@gmail.com>
2024-03-24 00:07:01 +0300

    Redefined the settings controller

--- lib/features/feature_settings/presentation/controller/settings_controller.dart
+++ lib/features/feature_settings/presentation/controller/settings_controller.dart
@@ -1,13 +1,6 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:get/get.dart';
 
-part 'settings_controller.g.dart';
+class SettingsController extends GetxController {
 
-@riverpod
-class SimCardToggle extends _$SimCardToggle {
-  @override
-  int build() => 1;
 
-  //  toggle sim card state
-  void setDefaultSimCard({required int defaultSim}) => state = defaultSim;
 }
