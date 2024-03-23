a24f1ab0c9475dd2e992aca46e905f10f80d30d0
KenStarry <starrycodes@gmail.com>
2024-03-24 01:32:37 +0300

    Settings Controller Use Cases

--- lib/features/feature_settings/presentation/controller/settings_controller.dart
+++ lib/features/feature_settings/presentation/controller/settings_controller.dart
@@ -1,6 +1,16 @@
 import 'package:get/get.dart';
+import 'package:mobile_number/mobile_number.dart';
+import 'package:okoa/di/di.dart';
+import 'package:okoa/features/feature_settings/domain/use_case/settings_use_cases.dart';
 
 class SettingsController extends GetxController {
+  final useCases = locator.get<SettingsUseCases>();
 
+  final simCards = <SimCard>[].obs;
 
+  /// Get Sim Cards
+  Future<void> getSimCards() async =>
+      await useCases.getSimCards.call(onSimCardsFetched: (sims) {
+        simCards.value = sims;
+      });
 }
