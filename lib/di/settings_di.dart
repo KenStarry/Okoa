1715f6d047f028c9e61d19a00decf84f1dfd6bca
KenStarry <starrycodes@gmail.com>
2024-03-24 01:30:38 +0300

    Get Sim Cards use case

--- lib/di/settings_di.dart
+++ lib/di/settings_di.dart
@@ -1,9 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:okoa/features/feature_settings/data/repository/settings_repository_impl.dart';
 import 'package:okoa/features/feature_settings/domain/repository/settings_repository.dart';
+import 'package:okoa/features/feature_settings/domain/use_case/get_sim_cards.dart';
+import 'package:okoa/features/feature_settings/domain/use_case/settings_use_cases.dart';
 
 void settingsDI({required GetIt locator}) {
   /// Settings Repo
   locator.registerLazySingleton<SettingsRepository>(
       () => SettingsRepositoryImpl());
+
+  /// Settings Use Cases
+  locator.registerLazySingleton<SettingsUseCases>(
+      () => SettingsUseCases(getSimCards: GetSimCards()));
 }
---
+++ lib/features/feature_settings/domain/use_case/settings_use_cases.dart
@@ -0,0 +1,7 @@
+import 'package:okoa/features/feature_settings/domain/use_case/get_sim_cards.dart';
+
+class SettingsUseCases {
+  final GetSimCards getSimCards;
+
+  const SettingsUseCases({required this.getSimCards});
+}
